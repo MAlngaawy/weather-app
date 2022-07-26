cec433b952bd3ba2ed332b10ed46698868e7d689
MAlngaawy <m.alngaawy@gmail.com>
2022-07-26 12:32:35 +0200

    add imports and middlewires

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express.js')
 // Start up an instance of app
+const app = express()
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,6 +12,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors())
 
 // Initialize the main project folder
 app.use(express.static('website'));
