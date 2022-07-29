92321cc9d8e69803f72bcb301c8b1f34db45e3ac
MAlgaawy <m.alngaawy@gmail.com>
2022-07-29 13:44:42 +0200

    handle heroku deployment

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static('website'));
 
 // Setup Server
 //set app the post
-const port = 8000
+const port = process.env.PORT || 8000
 app.listen(port, () => {
   console.log("It's work")
 })
